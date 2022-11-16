a4b0a3dfe43f13d9ae4c14785e514077ca5135f4
Gowther <AcevedoAriel@outlook.com>
2022-11-16 20:24:42 -0300

    PWA cfg parcial

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Provider } from 'react-redux'
-import { combineReducers, createStore } from 'redux'
-import { interactionReducer } from './reducers/interactionReducer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ReloadPrompt from "./ReloadPrompt";
+import "./index.css";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { interactionReducer } from "./reducers/interactionReducer";
 
-const reducers = combineReducers(
-  {
-    interaction: interactionReducer
-  }
-)
+const reducers = combineReducers({
+  interaction: interactionReducer,
+});
 
-const store = createStore(reducers)
+const store = createStore(reducers);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <ReloadPrompt />
     <App />
   </Provider>
-)
+);
